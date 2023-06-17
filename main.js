7cf34016c2ba0dcc14eb6fcef01c7b364c4ddf4a
Patrick Laughlin <prlaugh@gmail.com>
2023-06-16 21:23:21 -0400

    add a concise mode. has issues though it seems

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-
 require('dotenv').config();
-const chalk = require('chalk')
+const { program } = require('commander');
+const mustache = require('mustache');
+const chalk = require('chalk');
 const screenshot = require('screenshot-desktop');
 const Tesseract = require('tesseract.js');
 const fs = require('fs');
@@ -12,67 +13,82 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
+program.version('0.0.1');
+program.option('-c, --concise', 'concise mode');
+program.parse();
+
 const weights = {
-  "Critical Strike Chance": 9,
-  "Critical Strike Damage": 9,
-  "Core Skill Damage": 9,
-  "Maximum Essence": 8,
-  "Damage Reduction (all forms)": 8,
-  "Intelligence": 7,
-  "Dexterity": 6,
-  "Willpower": 6,
-  "Strength": 3
-}
+    "Critical Strike Chance": 9,
+    "Critical Strike Damage": 9,
+    "Core Skill Damage": 9,
+    "Maximum Essence": 8,
+    "Damage Reduction (all forms)": 8,
+    "Intelligence": 7,
+    "Dexterity": 6,
+    "Willpower": 6,
+    "Strength": 3
+};
 
-const prompt = (ocrOutput) => `
-I am playing Diablo 4 and these are my characters priority stats by weights in json format
-
-${JSON.stringify(weights)}
-
-the OCR output below is comparing two items. you will have to parse that text to determine what the two items and their stats are. the text does contain + or - on specific stats since it is a comparison.
-
-${ocrOutput}
-
-Taking into account the weighted stats i want and the text given, tell me which item i should equip for the best stats for my character.
-`
 
 screenshot().then((img) => {
-  // Save the screenshot to a file
-  fs.writeFile('screenshot.png', img, (err) => {
-    if (err) throw err;
-
-    // Perform OCR on the screenshot
-    Tesseract.recognize('screenshot.png', 'eng', { logger: (m) => console.log(m) })
-      .then(({ data: { text } }) => {
-
-        console.log('sending this prompt to Chat Completion API: ', prompt(text, weights));
-
-        openai.createChatCompletion({
-            model: 'gpt-4-0613',
-            messages: [
-                {
-                    'role': 'system',
-                    'content': 'You are an expert ARPG character stat analyzer who is well versed in action rpgs and the importance of stats.'
-                },
-                {
-                    'role': 'user',
-                    'content': prompt(text, weights)
+    // Save the screenshot to a file
+    fs.writeFile('screenshot.png', img, (err) => {
+        if (err) throw err;
+        
+        // Perform OCR on the screenshot
+        Tesseract.recognize('screenshot.png', 'eng', { logger: (m) => console.log(m) })
+        .then(({ data: { text } }) => {
+            const data = {
+                ocrOutput: text,
+                weights: JSON.stringify(weights),
+                concise: program.opts().concise,
+                conciseMessage: 'Only provide the name of the item to choose, do not explain your reasoning',
+            };
+            
+            const template = `
+            I am playing Diablo 4 and these are my character's priority stats by weights in JSON format
+            
+            {{{weights}}}
+            
+            The OCR output below is comparing two items. You will have to parse that text to determine what the two items and their stats are. The text does contain + or - on specific stats since it is a comparison.
+            
+            {{{ocrOutput}}}
+            
+            Taking into account the weighted stats I want and the text given, tell me which item I should equip for the best stats for my character.
+            {{#concise}}
+            {{{conciseMessage}}}
+            {{/concise}}
+            `;
+            
+            const renderedTemplate = mustache.render(template, data);
+            
+            console.log(chalk.green(renderedTemplate));
+            
+            openai.createChatCompletion({
+                model: 'gpt-4-0613',
+                messages: [
+                    {
+                        role: 'system',
+                        content: 'You are an expert ARPG character stat analyzer who is well versed in action RPGs and the importance of stats.'
+                    },
+                    {
+                        role: 'user',
+                        content: renderedTemplate
+                    }
+                ]
+            })
+            .then(completion => {
+                if (Array.isArray(completion.data.choices)) {
+                    return console.log(chalk.green(completion.data.choices[0].message?.content));
                 }
-            ]
-        }).then(completion => {
-            if (Array.isArray(completion.data.choices)) {
-                return console.log(chalk.green(completion.data.choices[0].message?.content));
-            }
-            return console.log(completion.data.choices)
-        }).catch(err => {
-            console.log(err);
+                return console.log(completion.data.choices);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+            
+            // Delete the screenshot file
+            fs.unlink('screenshot.png');
         });
-
-        // Delete the screenshot file
-        fs.unlink('screenshot.png', (err) => {
-          if (err) throw err;
-          console.log('Screenshot deleted');
-        });
-      });
-  });
+    });
 });
