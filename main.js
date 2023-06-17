9311713d8f944a7cbcbcd404761cfc8747427047
Patrick Laughlin <prlaugh@gmail.com>
2023-06-16 22:20:44 -0400

    move to async await

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const mustache = require('mustache');
 const chalk = require('chalk');
 const screenshot = require('screenshot-desktop');
 const Tesseract = require('tesseract.js');
-const fs = require('fs');
+const fs = require('fs').promises; // Use promises version of fs
 const { Configuration, OpenAIApi } = require('openai');
 const { GlobalKeyboardListener } = require("node-global-key-listener");
 
@@ -30,69 +30,58 @@ const weights = {
     "Strength": 3
 };
 
-function main() {
-    screenshot().then((img) => {
-        fs.writeFile('screenshot.png', img, (err) => {
-            if (err) throw err;
-            Tesseract.recognize('screenshot.png', 'eng', { logger: m => console.log(chalk.blue(m.progress)) })
-            .then(({ data: { text } }) => {
+async function main() {
+    try {
+        const img = await screenshot();
+        await fs.writeFile('screenshot.png', img);
+        
+        const { data: { text } } = await Tesseract.recognize('screenshot.png', 'eng', { logger: m => console.log(m) });
+        await fs.unlink('screenshot.png');
+        
+        const data = {
+            ocrOutput: text,
+            weights: JSON.stringify(weights),
+            concise: program.opts().concise,
+            conciseMessage: 'Only provide the name of the item to choose, do not explain your reasoning',
+        };
 
-                const data = {
-                    ocrOutput: text,
-                    weights: JSON.stringify(weights),
-                    concise: program.opts().concise,
-                    conciseMessage: 'Only provide the name of the item to choose, do not explain your reasoning',
-                };
-                
-                const template = `
-                I am playing Diablo 4 and these are my character's priority stats by weights in JSON format
-                
-                {{{weights}}}
-                
-                The OCR output below is comparing two items. You will have to parse that text to determine what the two items and their stats are. The text does contain + or - on specific stats since it is a comparison.
-                
-                {{{ocrOutput}}}
-                
-                Taking into account the weighted stats I want and the text given, tell me which item I should equip for the best stats for my character.
-                {{#concise}}
-                {{{conciseMessage}}}
-                {{/concise}}
-                `;
-                
-                const renderedTemplate = mustache.render(template, data);
-                
-                console.log(chalk.green(renderedTemplate));
-                
-                openai.createChatCompletion({
-                    model: 'gpt-4-0613',
-                    messages: [
-                        {
-                            role: 'system',
-                            content: 'You are an expert ARPG character stat analyzer who is well versed in action RPGs and the importance of stats.'
-                        },
-                        {
-                            role: 'user',
-                            content: renderedTemplate
-                        }
-                    ]
-                })
-                .then(completion => {
-                    if (Array.isArray(completion.data.choices)) {
-                        return console.log(chalk.green(completion.data.choices[0].message?.content));
-                    }
-                    return console.log(completion.data.choices);
-                })
-                .catch(err => {
-                    bar.stop();
-                    console.log(err);
-                });
-                
-                fs.unlink('screenshot.png', (err) => {
-                    if (err) throw err;
-                });
-            });
+        const template = `
+        I am playing Diablo 4 and these are my character's priority stats by weights in JSON format
+        {{{weights}}}
+        The OCR output below is comparing two items. You will have to parse that text to determine what the two items and their stats are. The text does contain + or - on specific stats since it is a comparison.
+        {{{ocrOutput}}}
+        Taking into account the weighted stats I want and the text given, tell me which item I should equip for the best stats for my character.
+        {{#concise}}
+        {{{conciseMessage}}}
+        {{/concise}}
+        `;
+        
+        const renderedTemplate = mustache.render(template, data);
+        console.log(chalk.green(renderedTemplate));
+        
+        console.log(chalk.blue('Generating response...'))
+        const completion = await openai.createChatCompletion({
+            model: 'gpt-4-0613',
+            messages: [
+                {
+                    role: 'system',
+                    content: 'You are an expert ARPG character stat analyzer who is well versed in action RPGs and the importance of stats.'
+                },
+                {
+                    role: 'user',
+                    content: renderedTemplate
+                }
+            ]
         });
-    });
+        
+        if (Array.isArray(completion.data.choices)) {
+            console.log(chalk.green(completion.data.choices[0].message?.content));
+        } else {
+            console.log(completion.data.choices);
+        }
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 v.addListener(function (e, down) {
